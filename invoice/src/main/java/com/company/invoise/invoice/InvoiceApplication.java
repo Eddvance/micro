ba1ca6c819562d8407b7bda7a7fb8a523a5b2dff
Edouard Leroy <leroyedouard@protonmail.com>
2024-06-05 14:46:44 +0200

    Reunion des 5 modules

---
+++ invoice/src/main/java/com/company/invoise/invoice/InvoiceApplication.java
@@ -0,0 +1,20 @@
+package com.company.invoise.invoice;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceApplication.class, args);
+	}
+
+	@Bean
+	public Hibernate5Module dataTypeHibernate5Module(){
+		return new Hibernate5Module();
+	}
+
+}
