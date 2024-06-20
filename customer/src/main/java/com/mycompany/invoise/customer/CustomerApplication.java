7495c5864400f125f264ec5056d05324447a2a61
Edouard Leroy <leroyedouard@protonmail.com>
2024-06-20 16:03:20 +0200

    INSTABLE les entites metiers et les donnees fin

--- customer/src/main/java/com/mycompany/invoise/customer/CustomerApplication.java
+++ customer/src/main/java/com/mycompany/invoise/customer/CustomerApplication.java
@@ -3,7 +3,6 @@ package com.mycompany.invoise.customer;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
